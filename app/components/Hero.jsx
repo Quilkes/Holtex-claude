a222c1ae1e0740ad7052a31a4547dea7acf17b70
quilkes <matinadekola33@gmail.com>
2025-05-16 11:33:56 +0100

    fix: adjust Hero component layout and update page container height for consistency

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
             className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:pb-56 lg:pt-40">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:pt-40">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our next round of funding.{" "}
